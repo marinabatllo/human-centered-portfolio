547639672f99de115b18696d1ee6fcde59d1c7f6
Marina Batllo <marinabatllo@users.noreply.github.com>
2026-02-16 22:35:29 +0100

    Eliminate horizontal overflow and ensure perfect screen fit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Navbar />
       <main className="flex-1 pt-16 lg:pt-20">
         {children}
